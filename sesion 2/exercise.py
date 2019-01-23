c2c6d2db51b253792ef021cf90f6fbea14060b06
Claudia Ramos <claudiaramosrincon@gmail.com>
2019-01-23 18:10:31 +0100

    This is the fibonacci.py exercise from sesion 1

---
+++ sesion 2/exercise.py
@@ -0,0 +1,8 @@
+l = 100
+print("The result of adding the first", l, "number will appear below")
+
+total_sum = 0
+for i in range(100):
+    total_sum = total_sum + i + 1
+
+print("The total sum is: ", total_sum)
