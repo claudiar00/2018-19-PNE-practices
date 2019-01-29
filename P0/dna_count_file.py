8b409dd188ebb6942e8d80da1f18fb91e2ca2985
Claudia Ramos <claudiaramosrincon@gmail.com>
2019-01-29 16:09:32 +0100

    PO projects

--- P0/dna_count_file.py
+++ P0/dna_count_file.py
@@ -1,11 +1,18 @@
 #The exercise 5 consists on reading several DNA sequences from an external file and counting the number of bases that
 #appears in them.
 
-with file(dna_sequence) as f:
-    write = f.write('ACTG', '\n', 'ACGT')
+with open ('dna.txt', 'w') as f:
+    f.write('AGTCAG')
+    f.write('ACGTAG')
+f.close()
+
 #Here we create a program that reads the wanted filename but we have ti create previously a filenme with some DNA
 #sequences in order to let the program read it and do what its asked.
-with file(dna_sequence) as f:
-    read = f.read()
+with open ('dna.txt', 'r') as f:
+    lenght = len(f.read())
+    print("The lenght of the sequence is: ", lenght)
+    for elem in f.read():
+        print ("hol")
+
 
 
