3c6711cf38b9a44d357e3089a25671886ec66cfc
Claudia Ramos <claudiaramosrincon@gmail.com>
2019-01-30 17:11:24 +0100

    dna count

--- P0/dna_count.py
+++ P0/dna_count.py
@@ -1,17 +1,12 @@
-sequence = print(input("Insert a dna sequence: "))
+sequence = (input("Insert a dna sequence: "))
+print(sequence)
 
+num_a = sequence.count('A')
+num_c = sequence.count('C')
+num_g = sequence.count('G')
+num_t = sequence.count('T')
 
-
-
-
-
-    num_a = sequence.count('A')
-    num_c = sequence.count('C')
-    num_g = sequence.count('G')
-    num_t = sequence.count('T')
-
-
-    print("Number of A's in the CPLX2 sequence: ", num_a)
-    print("Number of C's in the CPLX2 sequence: ", num_c)
-    print("Number of G's in the CPLX2 sequence: ", num_g)
-    print("Number of T's in the CPLX2 sequence: ", num_t)
+print("Number of A's in the CPLX2 sequence: ", num_a)
+print("Number of C's in the CPLX2 sequence: ", num_c)
+print("Number of G's in the CPLX2 sequence: ", num_g)
+print("Number of T's in the CPLX2 sequence: ", num_t)
