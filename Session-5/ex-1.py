eca6fb069ea1781bbf0152fc9d559342001aac4f
Claudia Ramos <claudiaramosrincon@gmail.com>
2019-02-05 15:50:16 +0100

    session 5 ex-1

--- Session-5/ex-1.py
+++ Session-5/ex-1.py
@@ -1,24 +1,27 @@
 def count_a(seq):
     counter = 0
     for elem in seq:
-        if elem == 'A':
+        if elem.upper() == 'A':
             counter += 1
     return counter
 
 
 # main program
-s = 'AGTCTAGCAGTAGTAGATAGATAGTACGATCGATCAGTCAGA'
+s = input('Please enter a valid sequence: ')
 number_a = count_a(s)
 print('The number of As in the sequence is: ', number_a)
 
 l = len(s)
 
-print('The length of the sequence is: ',l)
+if l > 0:
+    perc = round(100.0 * number_a / l, 1)
+else:
+    perc = 0
 
 #The percentage of As in the sequence
 
-perc = round(100.0*number_a/l, 1)
 
+print('The length of the sequence is: ',l)
 print('The percentage of As in the sequence is: ', perc, '%')
 
 
