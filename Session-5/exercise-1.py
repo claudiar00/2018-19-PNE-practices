abfbedf0fa26767e95ab49b8d454924ec5a5b795
Claudia Ramos <claudiaramosrincon@gmail.com>
2019-02-06 16:12:02 +0100

    exercise 1- session 5

--- Session-5/exercise-1.py
+++ Session-5/exercise-1.py
@@ -22,7 +22,7 @@ l = len(seq)
 for k in count_bases(seq).keys():
     if l > 0:
         perc = round(100.0*count_bases(seq)[k]/l, 1)
-        print('This sequence is', len(seq), 'bases in length', '\n')
+        print('This sequence is', l, 'bases in length', '\n')
         print('Base ',k, '\n', 'Counter: {}'.format(count_bases(seq)[k]))
         print(' Percentage: {}'.format(perc), '%')
 
