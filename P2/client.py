64e7041c3bdf94688a12d1c0031a0c4931c0d661
Claudia Ramos <claudiaramosrincon@gmail.com>
2019-02-15 10:47:22 +0100

    practice 2 client code

--- P2/client.py
+++ P2/client.py
@@ -3,17 +3,20 @@ from Seq import Seq
 
 #SERVER IP, PORT
 
-IP = '79.159.207.230'
-PORT = 8081
+IP = '127.0.0.1' #I have put here my computers IP
+PORT = 8090
 
 while True:
     sequence = input('Please insert a sequence: ')
     s1 = Seq(sequence)
-    s2 = s1.complement()
-
+    comp = s1.complement()
+    reverse = s1.reverse()
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((IP, PORT))
-    s.send(str.encode(s2.strbase()))
+    s.send(str.encode('\n The complement sequence: '))
+    s.send(str.encode(comp.strbase))
+    s.send(str.encode('\n The reverse sequence: '))
+    s.send(str.encode(reverse.strbase))
     msg = s.recv(2048).decode('utf-8')
     print('MESSAGE FROM THE SERVER: /n')
     print(msg)
